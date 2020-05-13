dd7e4bf9d349e50c11067eb9fb38f04e54dc4f6f
Meghamalak <meghamalakarnati26@icloud.com>
2020-05-12 17:27:45 -0700

    Weather Service

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ICurrentWeather } from '../icurrent-weather';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-current-weather',
   templateUrl: './current-weather.component.html',
   styleUrls: ['./current-weather.component.css']
 })
-export class CurrentWeatherComponent implements OnInit {
+export class CurrentWeatherComponent implements OnInit
+{
+  current: ICurrentWeather
 
-  constructor() { }
+  constructor(private weatherService: WeatherService)
+  {
 
-  ngOnInit(): void {
+  }
+
+  ngOnInit(): void
+  {
+    this.weatherService.getCurrentWeather('Texas', 'US').subscribe(data => this.current = data)
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  appId: '536ad11c7b00d4b3cedc41ebab1ecb5e',
+  baseUrl: 'http//'
 };
 
 /*
